ae1669e9fbe0a3639f54f978e9d5181a3a0438e8
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2023-10-21 13:22:56 +0100

    Update sort.h

--- sort.h
+++ sort.h
@@ -45,5 +45,6 @@ void selection_sort(int *array, size_t size);
 
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
 
 #endif /* SORT_H */
