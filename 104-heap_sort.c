52efd0984a7dcafbccb1e31a1beb0c6eb351be38
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2023-10-21 13:20:55 +0100

    Update 104-heap_sort.c implementation of max_heapify function

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -32,7 +32,23 @@ void swap_ints(int *a, int *b)
  */
 void max_heapify(int *array, size_t size, size_t base, size_t root)
 {
-  /* implementation */
+	size_t left, right, large;
+
+	left = 2 * root + 1;
+	right = 2 * root + 2;
+	large = root;
+
+	if (left < base && array[left] > array[large])
+		large = left;
+	if (right < base && array[right] > array[large])
+		large = right;
+
+	if (large != root)
+	{
+		swap_ints(array + root, array + large);
+		print_array(array, size);
+		max_heapify(array, size, base, large);
+	}
 }
 
 /**
