327d191fa1ca8d43b702f38fd62f5bfdb1e989bd
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2023-10-21 12:56:57 +0100

    Update 103-merge_sort.c implement merge_subarr function

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,6 +1,6 @@
 /*
  * File: 103-merge_sort.c
- * Auth: Brennan D Baraban
+ *
  */
 
 #include "sort.h"
@@ -21,7 +21,25 @@ void merge_sort(int *array, size_t size);
 void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
 		size_t back)
 {
-  /* implementation */
+	size_t i, j, k = 0;
+
+	printf("Merging...\n[left]: ");
+	print_array(subarr + front, mid - front);
+
+	printf("[right]: ");
+	print_array(subarr + mid, back - mid);
+
+	for (i = front, j = mid; i < mid && j < back; k++)
+		buff[k] = (subarr[i] < subarr[j]) ? subarr[i++] : subarr[j++];
+	for (; i < mid; i++)
+		buff[k++] = subarr[i];
+	for (; j < back; j++)
+		buff[k++] = subarr[j];
+	for (i = front, k = 0; i < back; i++)
+		subarr[i] = buff[k++];
+
+	printf("[Done]: ");
+	print_array(subarr + front, back - front);
 }
 
 /**
